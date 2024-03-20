eff22ec27943d46e120dc2541e9836036e4bd14b
seoyun03 <jangs370@newschool.edu>
2024-03-20 15:13:04 -0400

    update

--- on punctuation/four/script.js
+++
@@ -1,8 +0,0 @@
-function showText(className) {
-    var elements = document.querySelectorAll('.' + className);
-    elements.forEach(function(element) {
-      element.style.display = 'none';
-    });
-    var hoverText = document.querySelector('.hover-text.' + className);
-    hoverText.style.display = 'block';
-  }
