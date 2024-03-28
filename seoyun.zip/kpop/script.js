9c4696674392c02f639a65080e98b932c40e2885
seoyun03 <jangs370@newschool.edu>
2024-03-28 13:06:16 -0400

    update

--- seoyun.zip/kpop/script.js
+++ seoyun.zip/kpop/script.js
@@ -11,3 +11,14 @@ imageContainers.forEach(container => {
     image.style.display = 'none';
   });
 });
+function playAudio(element) {
+  var audio = element.querySelector('.audio');
+  audio.play();
+}
+
+// 마우스가 떼질 때 음악을 멈추는 함수
+function pauseAudio(element) {
+  var audio = element.querySelector('.audio');
+  audio.pause();
+  audio.currentTime = 0; // 재생 위치 초기화
+}
