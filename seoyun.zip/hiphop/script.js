904c4346be5197bf9eb8ee765b367eb5201309a0
seoyun03 <jangs370@newschool.edu>
2024-03-28 11:53:10 -0400

    update

--- seoyun.zip/hiphop/script.js
+++ seoyun.zip/hiphop/script.js
@@ -2,113 +2,113 @@ const jsonData = [
   {
       "title": "No Role Modelz",
       "artist": "J.Cole",
-      "when": "2024-03-08T05:00:00.000Z",
-      "where": "220 W 24th St",
+      "Date": "2024-03-08T05:00:00.000Z",
+      "Location": "220 W 24th St",
       "source": "Apple Music"
   },
   {
       "title": "What Would Pluto Do",
       "artist": "Drake",
-      "when": "2024-03-08T05:00:00.000Z",
-      "where": "220 W 24th St",
+      "Date": "2024-03-08T05:00:00.000Z",
+      "Location": "220 W 24th St",
       "source": "Apple Music"
   },
   {
       "title": "Talk to Me (feat. Drake)",
       "artist": "Drakeo the Ruler",
-      "when": "2024-03-08T05:00:00.000Z",
-      "where": "220 W 24th St",
+      "Date": "2024-03-08T05:00:00.000Z",
+      "Location": "220 W 24th St",
       "source": "Apple Music"
   },
   {
       "title": "Love Letter",
       "artist": "Frostydasnomann",
-      "when": "2024-03-10T05:00:00.000Z",
-      "where": "65 5th Ave",
+      "Date": "2024-03-10T05:00:00.000Z",
+      "Location": "65 5th Ave",
       "source": "Spotify"
   },
   {
       "title": "Rich Flex",
       "artist": "Drake & 21 Savage",
-      "when": "2024-03-11T04:00:00.000Z",
-      "where": "Los Angeles",
+      "Date": "2024-03-11T04:00:00.000Z",
+      "Location": "Los Angeles",
       "source": "Apple Music"
   },
   {
       "title": "Loyal (feat. Lil Wayne & Tyga)",
       "artist": "Chris Brown",
-      "when": "2024-03-11T04:00:00.000Z",
-      "where": "Los Angeles",
+      "Date": "2024-03-11T04:00:00.000Z",
+      "Location": "Los Angeles",
       "source": "Spotify"
   },
   {
       "title": "Bad Bad Bad",
       "artist": "Young Thug",
-      "when": "2024-03-12T04:00:00.000Z",
-      "where": "Los Angeles",
+      "Date": "2024-03-12T04:00:00.000Z",
+      "Location": "Los Angeles",
       "source": "Apple Music"
   },
   {
       "title": "You Got It",
       "artist": "VEDO",
-      "when": "2024-03-13T04:00:00.000Z",
-      "where": "Los Angeles",
+      "Date": "2024-03-13T04:00:00.000Z",
+      "Location": "Los Angeles",
       "source": "Instagram"
   },
   {
       "title": "Easy Breezy",
       "artist": "Kiana Ledé",
-      "when": "2024-03-14T04:00:00.000Z",
-      "where": "220 W 24th St",
+      "Date": "2024-03-14T04:00:00.000Z",
+      "Location": "220 W 24th St",
       "source": "Apple Music"
   },
   {
       "title": "Body a Canvas",
       "artist": "K CAMP",
-      "when": "2024-03-18T04:00:00.000Z",
-      "where": "Soho",
+      "Date": "2024-03-18T04:00:00.000Z",
+      "Location": "Soho",
       "source": "Spotify"
   },
   {
       "title": "I Choose",
       "artist": "Layton Greene",
-      "when": "2024-03-20T04:00:00.000Z",
-      "where": "65 5th Ave",
+      "Date": "2024-03-20T04:00:00.000Z",
+      "Location": "65 5th Ave",
       "source": "Apple Music"
   },
   {
       "title": "Make Me Wanna",
       "artist": "VEDO",
-      "when": "2024-03-20T04:00:00.000Z",
-      "where": "65 5th Ave",
+      "Date": "2024-03-20T04:00:00.000Z",
+      "Location": "65 5th Ave",
       "source": "Apple Music"
   },
   {
       "title": "You",
       "artist": "Jacquees",
-      "when": "2024-03-21T04:00:00.000Z",
-      "where": "220 W 24th St",
+      "Date": "2024-03-21T04:00:00.000Z",
+      "Location": "220 W 24th St",
       "source": "TikTok"
   },
   {
       "title": "For The Night",
       "artist": "Pop Smoke",
-      "when": "2024-03-22T04:00:00.000Z",
-      "where": "Soho",
+      "Date": "2024-03-22T04:00:00.000Z",
+      "Location": "Soho",
       "source": "Spotify"
   },
   {
       "title": "Go Crazy",
       "artist": "Chris Brown & Young Thug",
-      "when": "2024-03-24T04:00:00.000Z",
-      "where": "65 5th Ave",
+      "Date": "2024-03-24T04:00:00.000Z",
+      "Location": "65 5th Ave",
       "source": "Spotify"
   },
   {
       "title": "Nonstop",
       "artist": "Drake",
-      "when": "2024-03-24T04:00:00.000Z",
-      "where": "220 W 24th St",
+      "Date": "2024-03-24T04:00:00.000Z",
+      "Location": "220 W 24th St",
       "source": "Apple Music"
   }
 ]
@@ -121,8 +121,20 @@ images.forEach((image, index) => {
   tooltip.innerHTML = `
     <p>Title: ${data.title}</p>
     <p>Artist: ${data.artist}</p>
-    <p>When: ${new Date(data.when).toLocaleDateString()}</p>
-    <p>Where: ${data.where}</p>
+    <p>Date: ${new Date(data.Date).toLocaleDateString()}</p>
+    <p>Location: ${data.Location}</p>
     <p>Source: ${data.source}</p>
   `;
 });
+
+function playAudio(element) {
+    var audio = element.querySelector('.audio');
+    audio.play();
+  }
+  
+  // 마우스가 떼질 때 음악을 멈추는 함수
+  function pauseAudio(element) {
+    var audio = element.querySelector('.audio');
+    audio.pause();
+    audio.currentTime = 0; // 재생 위치 초기화
+  }
