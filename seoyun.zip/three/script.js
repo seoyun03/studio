4b78eba48e376bb0e8d2981ab1ad8b920ea8fd7f
seoyun03 <jangs370@newschool.edu>
2024-04-01 03:49:41 -0400

    update

--- seoyun.zip/three/script.js
+++ seoyun.zip/three/script.js
@@ -1,55 +1,61 @@
 document.addEventListener("DOMContentLoaded", function() {
   fetch("data.json")
-  .then((response) => response.json())
-  .then((items) => {
+  .then ((response) => response.json())
+  .then((items)=> {
+    console.log(items); // 데이터 로그로 확인
+    const musicData = items;
+    
     function filterMusic(genre) {
       const gridContainer = document.querySelector('.grid-container');
       gridContainer.innerHTML = ''; 
-
-      items.forEach(item => {
-        if (item.genre === genre) {
-          item.playlist.forEach(song => {
-            const gridItem = document.createElement('div');
-            gridItem.classList.add('grid-item');
-
-            gridItem.innerHTML = `
-              <img src="${song.image}" alt="${song.title}">
-              <audio class="audio" controls loop>
-                <source src="${song.audio}" type="audio/mpeg">
-              </audio>
-              <div class="caption">
-                <p><strong>Title:</strong> ${song.title}</p>
-                <p><strong>Artist:</strong> ${song.artist}</p>
-                <p><strong>Date:</strong> ${new Date(song.date).toLocaleDateString()}</p>
-                <p><strong>Location:</strong> ${song.location}</p>
-                <p><strong>Source:</strong> ${song.source}</p>
-              </div>
-            `;
-
-            gridContainer.appendChild(gridItem); 
-          });
-        }
+  
+      console.log('musicData[genre]', musicData[genre]);
+  
+      musicData[genre].playlist.forEach(song => {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+  
+        gridItem.innerHTML = `
+        <img src="${song.image}" alt="${song.title}">
+        <audio class="audio" controls loop>
+          <source src="${song.audio}" type="audio/mpeg">
+        </audio>
+        <div class="caption">
+          <p><strong>Title:</strong> ${song.title}</p>
+          <p><strong>Artist:</strong> ${song.artist}</p>
+          <p><strong>Date:</strong> ${new Date(song.date).toLocaleDateString()}</p>
+          <p><strong>Location:</strong> ${song.location}</p>
+          <p><strong>Source:</strong> ${song.source}</p>
+        </div>
+      `;
+  
+        gridContainer.appendChild(gridItem); 
       });
     }
-
+  
+  
     const filterButtons = document.querySelectorAll('.filter a');
     filterButtons.forEach(button => {
       button.addEventListener('click', function(event) {
         event.preventDefault(); 
-        const genre = this.getAttribute('data-genre');
+        const genre = parseInt(this.dataset.index);
+        console.log(genre); // 장르 로그로 확인
         filterMusic(genre); 
       });
     });
   });
 });
 
-function playAudio(element) {
-  var audio = element.querySelector('.audio');
-  audio.play();
-}
-
-function pauseAudio(element) {
-  var audio = element.querySelector('.audio');
-  audio.pause();
-  audio.currentTime = 0; 
-}
+  
+  
+  
+      function playAudio(element) {
+              var audio = element.querySelector('.audio');
+              audio.play();
+            }
+            
+            function pauseAudio(element) {
+              var audio = element.querySelector('.audio');
+              audio.pause();
+              audio.currentTime = 0; 
+            }
